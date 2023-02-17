447fa84afce59d2981cbac2d94f7e96795affcfa
Cooper <coopergum@gmail.com>
2023-02-17 12:55:32 +1100

    Bullet count enemy death and partly borders

--- Player/Player.gd
+++ Player/Player.gd
@@ -9,16 +9,21 @@ func _ready():
 
 func _process(delta):
 	if Input.is_action_just_pressed("fire"):
+		GlobalVariables.bulletInstanceCount += 1
 		var bulletInstance = bulletSource.instance()
 		bulletInstance.position = Vector2(position.x, position.y-20)
 		get_tree().get_root().add_child(bulletInstance)
 		
 func _physics_process(delta):
 	if Input.is_action_pressed("ui_left"):
-		move_and_collide(Vector2(-movement_speed * delta, 0))
+		if position.x > 10:
+			move_and_collide(Vector2(-movement_speed * delta, 0))
 	if Input.is_action_pressed("ui_right"):
-		move_and_collide(Vector2(movement_speed * delta, 0))
-	if Input.is_action_just_pressed("ui_down"):
-		move_and_collide(Vector2(0, movement_speed * delta))
-	if Input.is_action_just_pressed("ui_up"):
+		if position.x < 1230:
+			move_and_collide(Vector2(movement_speed * delta, 0))
+	if Input.is_action_pressed("ui_down"):
+		if position.y < 1:
+			move_and_collide(Vector2(0, movement_speed * delta))
+	if Input.is_action_pressed("ui_up"):
+		
 		move_and_collide(Vector2(0, -movement_speed * delta))
