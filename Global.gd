46308c90008682ccfc95d6c4ba8d19b6f659e15c
Cooper <coopergum@gmail.com>
2023-03-06 14:45:07 +1100

    Pete Davidson

--- Global.gd
+++ Global.gd
@@ -3,3 +3,10 @@ extends Node
 var bulletInstanceCount = 0
 var enemyBulletInstanceCount = 0
 var automaticFiring = false
+
+var scoringInformation = {
+	"currentScore": 0,
+	"currentPlayer": "User",
+	"highScore": 0,
+	"highScorePlayersName" : "Winner"
+}
