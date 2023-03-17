4825ad560943f87688caf5cb225c0d75ba463b47
Cooper <coopergum@gmail.com>
2023-03-17 12:51:25 +1100

    stuff

---
+++ Menu/Menu.gd
@@ -0,0 +1,10 @@
+extends Control
+
+func _ready():
+	var file = File.new()
+	if file.file_exists(GlobalVariables.saveFile):
+		var error = file.open(GlobalVariables.saveFile, File.READ)
+		if error == OK:
+			var player_data = file.get_var()
+			file.close()
+			GlobalVariables.scoringInformation = player_data
